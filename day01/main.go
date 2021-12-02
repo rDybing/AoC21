fa529bf9b3c8bd2d962f7d6bc267bf64a03b2a45
Roy Dybing <roy.dybing@gmail.com>
2021-12-02 04:02:13 +0100

    day01p2

--- day01/main.go
+++ day01/main.go
@@ -10,8 +10,8 @@ import (
 )
 
 type dataT struct {
-	depth    int
-	increase bool
+	depth int
+	sum   int
 }
 
 func main() {
@@ -19,22 +19,29 @@ func main() {
 	if err != nil {
 		log.Panicf("Failed loading data file: %v\n", err)
 	}
-	var incCount int
+	var incCount, sumCount int
 	start := time.Now()
 	for i, v := range d {
 		if i != 0 {
 			if v.depth > d[i-1].depth {
-				v.increase = true
 				incCount++
 			}
-		} else {
-			v.increase = false
+		}
+		if i > 1 {
+			v.sum = v.depth + d[i-1].depth + d[i-2].depth
+		}
+		if i > 2 {
+			if v.sum > d[i-1].sum {
+				sumCount++
+			}
 		}
 		d[i] = v
 	}
 	done := time.Now()
 	diff := done.Sub(start)
-	fmt.Printf("Found %d depth increases in %f seconds\n", incCount, diff.Seconds())
+	fmt.Printf("Part 1: Found %d depth increases\n", incCount)
+	fmt.Printf("Part 2: Found %d three-sum sliding increases\n", sumCount)
+	fmt.Printf("Execution time: %f\n", diff.Seconds())
 }
 
 func importData() ([]dataT, error) {
@@ -47,8 +54,8 @@ func importData() ([]dataT, error) {
 	for _, v := range arr {
 		depth, _ := strconv.Atoi(v)
 		dT := dataT{
-			depth:    depth,
-			increase: false,
+			depth: depth,
+			sum:   0,
 		}
 		d = append(d, dT)
 	}
